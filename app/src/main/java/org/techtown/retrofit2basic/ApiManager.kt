1d585c810b3023c259a6d4b0766afe005bbd611b
jiseongkimu <jiseongkimu@gmail.com>
2022-07-05 14:15:49 +0900

    뷰홀더가 이젠 말을 잘 들어요

--- app/src/main/java/org/techtown/retrofit2basic/ApiManager.kt
+++ app/src/main/java/org/techtown/retrofit2basic/ApiManager.kt
@@ -4,16 +4,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.create
-import java.security.SecureRandom
-import java.security.cert.X509Certificate
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
 import com.google.gson.GsonBuilder
-
-import com.google.gson.Gson
 
 object ApiManager {
 
@@ -39,32 +30,6 @@ object ApiManager {
         return instance
     }
 
-//    fun getUnsafeOkHttpClient(): OkHttpClient.Builder {
-//        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-//            override fun checkClientTrusted(p0: Array<out X509Certificate>?, authType: String?) {
-//
-//            }
-//
-//            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
-//
-//            }
-//
-//            override fun getAcceptedIssuers(): Array<X509Certificate> {
-//                return arrayOf()
-//            }
-//        })
-//
-//        val sslContext = SSLContext.getInstance("SSL")
-//        sslContext.init(null, trustAllCerts, SecureRandom())
-//
-//        val sslSocketFactory = sslContext.socketFactory
-//
-//        val builder = OkHttpClient.Builder()
-//        builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
-//        builder.hostnameVerifier { hostname, session -> true }
-//
-//        return builder
-//    }
     var gson = GsonBuilder()
         .setLenient()
         .create()
