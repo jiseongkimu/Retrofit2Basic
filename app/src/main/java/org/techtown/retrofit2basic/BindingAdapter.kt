ea635061619e4e9393b3cf8439e32b6988d099a3
jiseongkimu <jiseongkimu@gmail.com>
2022-07-06 11:53:39 +0900

    RecyclerView visibility btn

---
+++ app/src/main/java/org/techtown/retrofit2basic/BindingAdapter.kt
@@ -0,0 +1,41 @@
+package org.techtown.retrofit2basic
+
+import android.view.View
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
+object BindingAdapter {
+    @JvmStatic
+    @BindingAdapter("createDt")
+    fun setText1(view: TextView, text: String){
+        view.text = "createDt : " + text
+    }
+
+    @JvmStatic
+    @BindingAdapter("decideCnt")
+    fun setText2(view: TextView, text: String){
+        view.text = "decideCnt : " + text
+    }
+
+    @JvmStatic
+    @BindingAdapter("deathCnt")
+    fun setText3(view: TextView, text: String){
+        view.text = "deathCnt : " + text
+    }
+
+    @JvmStatic
+    @BindingAdapter("visible")
+    fun isVisible(view: View, isGone: Boolean) {
+        view.visibility = if (isGone) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("setVisiblrBtnText")
+    fun setVisiblrBtnText(view: TextView, isGone: Boolean) {
+        view.text = if(isGone) "INVISIBLE" else "VISIBLE"
+    }
+}
