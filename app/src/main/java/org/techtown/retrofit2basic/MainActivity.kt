78e7c5c605bdc078ac7c658d68008a351603e8e9
jiseongkimu <jiseongkimu@gmail.com>
2022-07-06 11:10:29 +0900

    RecyclerView visibility btn

--- app/src/main/java/org/techtown/retrofit2basic/MainActivity.kt
+++ app/src/main/java/org/techtown/retrofit2basic/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import org.techtown.retrofit2basic.databinding.ActivityMainBinding
+import org.techtown.retrofit2basic.databinding.ItemViewBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,6 +14,7 @@ import retrofit2.Retrofit
 class MainActivity : AppCompatActivity() {
     private val api_key =
         ""
+
     private val adapter by lazy { Adapter() }
 
     private lateinit var binding: ActivityMainBinding
@@ -21,9 +23,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+
         binding.recyclerView.adapter = adapter
 
-        ApiManager.getIntance().getData(api_key, 1, 10, 20200310, 20200315, "json").enqueue((object :
+        binding.loading = true
+
+        binding.btnInvisible.setOnClickListener{
+            binding.loading = false
+        }
+
+        binding.btnVisible.setOnClickListener{
+            binding.loading = true
+        }
+
+        binding.btnVisiblable.setOnClickListener {
+            if(binding.loading == false) binding.loading = true
+            else binding.loading = false
+        }
+
+        ApiManager.getIntance().getData(api_key, 1, 10, 20200110, 20200615, "json").enqueue((object :
             Callback<DataClass> {
             override fun onResponse(call: Call<DataClass>, response: Response<DataClass>) {
                 Log.d("TAG", "onResponse : " + response.body().toString())
@@ -31,7 +49,7 @@ class MainActivity : AppCompatActivity() {
                     Log.d("TAG", "response.isSuccessful : " + response.body().toString())
                     response.body()?.let {
 
-                        adapter.submitList( it.response.body.items.item) // 어댑터 메소드에 리스트 넘겨줌
+                        adapter.submitList(it.response.body.items.item) // 어댑터 메소드에 리스트 넘겨줌
                         adapter.notifyDataSetChanged() // 어댑터 갱신하라고 츠발라마 하는 코드
 
 //                        val adapter = Adapter(listOf(it))
@@ -45,5 +63,7 @@ class MainActivity : AppCompatActivity() {
                 Log.d("TAG", "응 안됐어" + t.message.toString())
             }
         }))
+
+
     }
 }
