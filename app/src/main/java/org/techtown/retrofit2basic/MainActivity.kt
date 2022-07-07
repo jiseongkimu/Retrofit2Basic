3f94f83bd29f7d56b8b67e65ecff33bce12d10c3
jiseongkimu <jiseongkimu@gmail.com>
2022-07-07 15:27:29 +0900

    ViewModel을 이용한 텍스트 뷰와 리사이클러뷰 토글

--- app/src/main/java/org/techtown/retrofit2basic/MainActivity.kt
+++ app/src/main/java/org/techtown/retrofit2basic/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import org.techtown.retrofit2basic.databinding.ActivityMainBinding
 import org.techtown.retrofit2basic.databinding.ItemViewBinding
 import retrofit2.Call
@@ -13,19 +14,34 @@ import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
     private val api_key =
-        ""
+        "YyaJKps0/kekxwrrVHBWQ7og4zx/ABO7rPKD+wTMWfcZp3C4vBVp502qXVGBgNOKTWPGdmuMDR6M7Ei3SyfBTA=="
 
     private val adapter by lazy { Adapter() }
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+        mainViewModel.isVisible.observe(this){isVisible ->
+            if(isVisible){
+                binding.visibleTxt.text = "숨김"
+                binding.loading = true
+            }
+            else {
+                binding.visibleTxt.text = "표시"
+                binding.loading = false
+            }
+        }
+
+        with(binding){
+            viewModel = mainViewModel
+        }
 
         binding.recyclerView.adapter = adapter
-
         binding.loading = false
 
         binding.btnInvisible.setOnClickListener{
@@ -34,11 +50,6 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnVisible.setOnClickListener{
             binding.loading = true
-        }
-
-        binding.btnVisiblable.setOnClickListener {
-            if(binding.loading == false) binding.loading = true
-            else binding.loading = false
         }
 
         ApiManager.getIntance().getData(api_key, 1, 10, 20200110, 20200615, "json").enqueue((object :
